151edb7187379d5840f89eb0921e345417d4668a
unknown <c0739472@mylambton.ca>
2022-05-30 00:17:25 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 const app = express();
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
+const path = require("path");
 
-app.use(cors({
-    origin: '*',
-}));
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 
-app.engine('hbs', handlebars.engine({
-    layoutsDir: __dirname + '/views/',
-    extname: 'hbs'
-    }));
-app.set('view engine', 'hbs');
-app.use(express.static('public'));
-
+app.engine(
+	"hbs",
+	handlebars.engine({
+		layoutsDir: __dirname + "/views/",
+		extname: "hbs",
+	})
+);
+app.set("view engine", "hbs");
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 
 //connect to database
 connectDB();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Server is running');
-})
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/login", require("./routes/api/login"));
+app.use("/api/download", require("./download"));
+app.use("/api/myresume", require("./routes/api/myresume"));
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/login', require('./routes/api/login'));
-app.use('/api/download', require('./download'));
-app.use('/api/myresume', require('./routes/api/myresume'));
+//serve static assets in productions
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at port : ${PORT}`);
+if (process.env.NODE_ENV === "production") {
+	//static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+app.listen(PORT, () => {
+	console.log(`Server is running at port : ${PORT}`);
 });
